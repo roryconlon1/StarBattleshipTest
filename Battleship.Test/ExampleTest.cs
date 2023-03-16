c2405c595890f0c556545ab303d84a535e4b9c13
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2023-03-16 22:52:48 +0000

    copied from java to csharp

--- Battleship.Test/ExampleTest.cs
+++ Battleship.Test/ExampleTest.cs
@@ -3,11 +3,72 @@ namespace Battleship.Test
     public class ExampleTest
     {
         [Fact]
-        public void TestPlay()
+        public void checkShips()
         {
-            var ships = new[] { "3:2,3:5" };
-            var guesses = new[] { "7:0", "3:3" };
-            Game.Play(ships, guesses).Should().Be(0);
+            var ships = new[] { "0:0,0:2", "2:2,2:3", "4:4,6:4", "6:1,9:1" };
+            Battleship.Game.SetUpGame(ships);
+            Assert.Equal(1, Battleship.Game.gameBoard[0, 0]);
+            Assert.Equal(1, Battleship.Game.gameBoard[0, 1]);
+            Assert.Equal(1, Battleship.Game.gameBoard[0, 2]);
+            Assert.Equal(2, Battleship.Game.gameBoard[2, 2]);
+            Assert.Equal(2, Battleship.Game.gameBoard[2, 3]);
+            Assert.Equal(3, Battleship.Game.gameBoard[4, 4]);
+            Assert.Equal(3, Battleship.Game.gameBoard[5, 4]);
+            Assert.Equal(3, Battleship.Game.gameBoard[6, 4]);
+            Assert.Equal(4, Battleship.Game.gameBoard[9, 1]);
+            Assert.Equal(4, Battleship.Game.gameBoard[8, 1]);
+            Assert.Equal(4, Battleship.Game.gameBoard[7, 1]);
+            Assert.Equal(4, Battleship.Game.gameBoard[6, 1]);
+        }
+
+        [Fact]
+        public void checkWater()
+        {
+            var ships = new[] { "0:0,0:2", "2:2,2:3", "4:4,6:4", "6:1,9:1" };
+            Battleship.Game.SetUpGame(ships);
+            Assert.Equal(0, Battleship.Game.gameBoard[1, 1]);
+            Assert.Equal(0, Battleship.Game.gameBoard[3, 7]);
+            Assert.Equal(0, Battleship.Game.gameBoard[9, 9]);
+        }
+
+        [Fact]
+        public void canGetHit()
+        {
+            var ships = new[] { "0:0,0:2", "2:2,2:3", "4:4,6:4", "6:1,9:1" };
+            Battleship.Game.SetUpGame(ships);
+            Assert.True(Battleship.Game.HitOrMiss("6:4"));
+            Assert.True(Battleship.Game.HitOrMiss("9:1"));
+            Assert.True(Battleship.Game.HitOrMiss("0:0"));
+        }
+
+        [Fact]
+        public void canReturnNotHit()
+        {
+            var ships = new[] { "0:0,0:2", "2:2,2:3", "4:4,6:4", "6:1,9:1" };
+            Battleship.Game.SetUpGame(ships);
+            Assert.False(Battleship.Game.HitOrMiss("3:7"));
+            Assert.False(Battleship.Game.HitOrMiss("1:1"));
+            Assert.False(Battleship.Game.HitOrMiss("9:9"));
+        }
+
+        [Fact]
+        public void canBeSunk()
+        {
+            var ships = new[] { "0:0,0:2", "2:2,2:3", "4:4,6:4", "6:1,9:1" };
+            Battleship.Game.SetUpGame(ships);
+            Assert.True(Battleship.Game.IsSunk(5));
+            Assert.False(Battleship.Game.IsSunk(2));
+        }
+
+        [Fact]
+        public void fullGameTest()
+        {
+            var ships = new[] { "0:0,0:2", "2:2,2:3", "4:4,6:4", "6:1,9:1" };
+            var guesses1 = new[] { "1:1", "2:2", "1:4", "2:3", "8:9", "4:4", "5:4", "6:4" };
+            var guesses2 = new[] { "1:1", "1:2", "1:4", "2:3", "8:9", "4:4", "5:4", "6:4" };
+            Battleship.Game.SetUpGame(ships);
+            Battleship.Game.Play(ships, guesses1).Should().Be(2);
+            Battleship.Game.Play(ships, guesses2).Should().Be(1);
         }
     }
 }
--- Battleship/Game.cs
+++ Battleship/Game.cs
@@ -12,9 +12,116 @@
         // guesses: each string represents the co-ordinate of a guess
         //   e.g. "7:0" - misses the ship above, "3:3" hits it.
         // returns: the number of ships sunk by the set of guesses
-        public static int Play(string[] ships, string[] guesses)
+
+        public static int[,] gameBoard;
+
+        public static void SetUpGame(String[] ships)
         {
-            return 0;
+            //            2d array to make a 10 by 10 game board of all value 0, whereby can change value depending on if
+            //            it contains a ship, if the ship has been hit or miss etc. and if the ship is sunk or not
+            gameBoard = new int[10, 10];
+            int shipsCount = 0;
+            for (int i = 0; i < ships.Length; i++)
+            {
+                //             for each ship in the array split by the comma to get the start and end points of the ship
+                //            from this, split coordinate of start/end by colon and find x and y of both
+                String[] individualShip = ships[i].Split(',');
+                String[] startCoordinates = individualShip[0].Split(':');
+                String[] endCoordinates = individualShip[1].Split(":");
+                int startXposition = int.Parse(startCoordinates[0]);
+                int endXposition = int.Parse(endCoordinates[0]);
+                int startYpositon = int.Parse(startCoordinates[1]);
+                int endYposition = int.Parse(endCoordinates[1]);
+                if (startXposition > 9 || endXposition > 9 || startYpositon > 9 || endYposition > 9)
+                {
+                    throw new IndexOutOfRangeException("Ship Placement out of bounds");
+                }
+                //              for each new ship, give it a new id by incrementing the value of how many total ships there are
+                //            determine if horizontal or vertical and assign the value of those positions to the same as the ships id
+                int shipId = shipsCount + 1;
+                if (startXposition != endXposition && startYpositon != endYposition)
+                {
+                    throw new ArgumentException("Invalid Ship Placement");
+                }
+                //               vertical
+                if (startXposition == endXposition)
+                {
+                    for (int j = startYpositon; j <= endYposition; j++)
+                    {
+                        gameBoard[startXposition, j] = shipId;
+                    }
+                }
+                else
+                {
+                    //              horizontal
+                    for (int l = startXposition; l <= endXposition; l++)
+                    {
+                        gameBoard[l, startYpositon] = shipId;
+                    }
+                }
+                shipsCount++;
+            }
+        }
+        //        check if an individual guess is a hit or not by checking if it is 0
+        //        or if it is greater and therefore a ship, returning bool value
+        public static bool HitOrMiss(String guess)
+        {
+            String[] guessCoordinates = guess.Split(':');
+            int x = int.Parse(guessCoordinates[0]);
+            int y = int.Parse(guessCoordinates[1]);
+            if (x > 9 || y > 9)
+            {
+                throw new IndexOutOfRangeException("Guessed Position out of bounds");
+            }
+            return gameBoard[x, y] > 0;
+        }
+
+        //        method to check if a ship is sunk, iterate over every element and if none match
+        //        a certain shipId then that ship is sunk
+
+        public static bool IsSunk(int shipId)
+        {
+            for (int i = 0; i < gameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameBoard.GetLength(1); j++)
+                {
+                    if (gameBoard[i, j] == shipId)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static int Play(String[] ships, String[] guesses)
+        {
+            SetUpGame(ships);
+            int boatsSunk = 0;
+            for (int i = 0; i < guesses.Length; i++)
+            {
+                if (HitOrMiss(guesses[i]))
+                {
+                    String[] guessCoordinates = guesses[i].Split(':');
+                    int x = int.Parse(guessCoordinates[0]);
+                    int y = int.Parse(guessCoordinates[1]);
+
+                    int idTracker = gameBoard[x, y];
+                    gameBoard[x, y] = -1;
+                    if (IsSunk(idTracker))
+                    {
+                        boatsSunk++;
+                    }
+                }
+                else
+                {
+                    String[] guessCoordinates = guesses[i].Split(':');
+                    int x = int.Parse(guessCoordinates[0]);
+                    int y = int.Parse(guessCoordinates[1]);
+                    gameBoard[x, y] = -2;
+                }
+            }
+            return boatsSunk;
         }
     }
 }
